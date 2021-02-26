37e093d15a7cb90a38d60aedaebdd4b5b000a0c3
feitosajoas <joasfeitosadelimasilva@gmail.com>
2021-02-25 22:16:13 -0300

    Finalizando header, trabalharei nele novamente depois

--- site/src/app/core/core.module.ts
+++ site/src/app/core/core.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
 @NgModule({
   declarations: [
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent
   ],
   imports: [
     CommonModule
